2356af79b6d3c14db9052d2a474c507e03f3a9ea
Ho Nhan <hothanhnhan24@gmail.com>
2023-12-04 14:21:04 +0700

    1-9-XLTBD: add lint & prettier, update markers

--- public/js/adBoard.js
+++ public/js/adBoard.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const valueSpan = document.querySelector('#buttonName');
-    if (valueSpan) {
-        const text = valueSpan.textContent;
-        const uppercaseText = text.toUpperCase();
-        valueSpan.textContent = uppercaseText;
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const valueSpan = document.querySelector("#buttonName");
+  if (valueSpan) {
+    const text = valueSpan.textContent;
+    const uppercaseText = text.toUpperCase();
+    valueSpan.textContent = uppercaseText;
+  }
 });
