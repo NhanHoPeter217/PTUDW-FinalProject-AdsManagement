95da179e1d8a352d56548fae228d3630d35aa141
Hai Nam <ddhnam21@clc.fitus.edu.vn>
2023-12-09 10:13:19 +0700

    nam work

--- public/js/utils.maps.js
+++ public/js/utils.maps.js
@@ -79,12 +79,15 @@ async function initMap() {
     streetView: false,
     streetViewCotrol: false,
     streetViewControlOptions: false,
+    clickableIcons: false
   });
 
   // The markers of all locations
   let markers = Array(locations.length);
-  let infoWindows = Array(locations.length);
-  let oldInfoWindow = undefined;
+
+  // Initial Infowindow for places
+  let infoWindow = new google.maps.InfoWindow();
+
   const parser = new DOMParser();
 
   // Ads' locations have been planned already
@@ -111,46 +114,43 @@ async function initMap() {
       title: location.title,
     });
 
-    infoWindows[index] = new google.maps.InfoWindow({
-      content: `<style>
-      .info-board {
-        background-color: var(--LightGreen);
-        font-family: Inter;
-      }
-    </style>
-    
-    <div class="card info-board" style="width: 5rem;">
-      <div class="card-body d-flex flex-row justify-content-around">
-        <svg width="29" height="34" viewBox="0 0 29 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g>
-            <path id="" d="M26.5455 14.2727C26.5455 23.8182 14.2727 32 14.2727 32C14.2727 32 2 23.8182 2 14.2727C2 11.0178 3.29302 7.89618 5.5946 5.5946C7.89618 3.29302 11.0178 2 14.2727 2C17.5277 2 20.6493 3.29302 22.9509 5.5946C25.2524 7.89618 26.5455 11.0178 26.5455 14.2727Z" stroke="var(--Green2)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-            <path id="" d="M14.2727 18.3636C16.532 18.3636 18.3636 16.5321 18.3636 14.2727C18.3636 12.0134 16.532 10.1818 14.2727 10.1818C12.0133 10.1818 10.1818 12.0134 10.1818 14.2727C10.1818 16.5321 12.0133 18.3636 14.2727 18.3636Z" stroke="var(--Green2)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-          </g>
-        </svg>
-    
-        <div class="d-flex flex-column">
-          <h5 class="card-title">Chưa có dữ liệu</h5>
-          <p class="card-text">Vui lòng chọn địa điểm khác</p>
-        </div>
-      </div>
-    
-    </div>`,
-      ariaLabel: location.title,
-    });
-
     markers[index].addListener("click", () => {
-      infoWindows[index].open({
+      // Close old Infowindow
+      infoWindow.close();
+      infoWindow = new google.maps.InfoWindow({
+        content: `<style>
+        .info-board {
+          background-color: var(--LightGreen);
+          font-family: Inter;
+        }
+      </style>
+      
+      <div class="card info-board" style="width: 5rem;">
+        <div class="card-body d-flex flex-row justify-content-around">
+          <svg width="29" height="34" viewBox="0 0 29 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g>
+              <path id="" d="M26.5455 14.2727C26.5455 23.8182 14.2727 32 14.2727 32C14.2727 32 2 23.8182 2 14.2727C2 11.0178 3.29302 7.89618 5.5946 5.5946C7.89618 3.29302 11.0178 2 14.2727 2C17.5277 2 20.6493 3.29302 22.9509 5.5946C25.2524 7.89618 26.5455 11.0178 26.5455 14.2727Z" stroke="var(--Green2)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+              <path id="" d="M14.2727 18.3636C16.532 18.3636 18.3636 16.5321 18.3636 14.2727C18.3636 12.0134 16.532 10.1818 14.2727 10.1818C12.0133 10.1818 10.1818 12.0134 10.1818 14.2727C10.1818 16.5321 12.0133 18.3636 14.2727 18.3636Z" stroke="var(--Green2)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+          </svg>
+      
+          <div class="d-flex flex-column">
+            <h5 class="card-title">Chưa có dữ liệu</h5>
+            <p class="card-text">Vui lòng chọn địa điểm khác</p>
+          </div>
+        </div>
+      
+      </div>`,
+        ariaLabel: location.title,
+      });
+
+      infoWindow.open({
         anchor: markers[index],
         map,
       });
 
-      map.panTo(location.coords);
-      if (oldInfoWindow != undefined) {
-        oldInfoWindow.close();
-        oldInfoWindow = infoWindows[index];
-      } else {
-        oldInfoWindow = infoWindows[index];
-      }
+      // Pan the map to the info window location
+      map.panToBounds(map.getBounds(), 1500);
     });
   });
 
@@ -214,6 +214,7 @@ function initAutocomplete() {
           position: place.geometry.location,
         }),
       );
+
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
@@ -223,6 +224,56 @@ function initAutocomplete() {
     });
     map.fitBounds(bounds);
   });
+
+
+  // Get places on click
+  let place_info = new google.maps.InfoWindow();
+  let geocoder = new google.maps.Geocoder();
+  let place_marker = new google.maps.Marker({
+      map: null,
+    });
+  let place_service = new google.maps.places.PlacesService(map);
+
+  map.addListener('click', (event)=>{
+    place_marker.setMap(null);
+    geocodeLatLng(geocoder, map, event.latLng);
+  });
+
+  function geocodeLatLng(geocoder, map, latlng) {
+    // Geocode to find the place_id of the place
+    geocoder
+      .geocode({ location: latlng })
+      .then((response) => {
+        if (response.results[0]) {
+          place_marker.setMap(map);
+          place_marker.setPosition(latlng);
+
+          let address_id = response.results[0].place_id;
+          let address = response.results[0].formatted_address;
+
+          let place_request = {
+            placeId: address_id,
+            fields: ["name"],
+          }
+          
+          // getDetails to get the name of the place
+          place_service.getDetails(place_request, function(result, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+              place_info.setContent(result.name);
+              place_info.open(map, place_marker);
+            }
+            else{
+              place_info.setContent(address);
+              place_info.open(map, place_marker);
+            }
+          });
+
+        } else {
+          window.alert("No results found");
+        }
+      })
+      .catch((e) => window.alert("Geocoder failed due to: " + e));
+  }
 }
 
 initMap();
