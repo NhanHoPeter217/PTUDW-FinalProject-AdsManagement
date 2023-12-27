07456dca69d6f761fe9dd95a3c57bd30ff4fb45e
Hai Nam <ddhnam21@clc.fitus.edu.vn>
2023-12-27 12:04:09 +0700

    add dev data generator files

---
+++ CANBO/GenerateData.js
@@ -0,0 +1,99 @@
+const fs = require("fs");
+require("dotenv").config();
+const connectDB = require("./db/connect");
+
+const AdvertisingBoard = require("./models/AdvertisingBoard");
+const AdvertisingPoint = require("./models/AdvertisingPoint");
+const LocationModel = require("./models/Location");
+const { AdvertisingFormat } = require("./models/Department/EnumList");
+const mongoose = require("mongoose");
+
+/* Mô tả dữ liệu như sau:
+
+    1 LOCATION là 'Trung tâm thương mại', tương ứng với 1 POINT ,
+    Có 10 BOARD rải khắp bên trong, mỗi BOARD có quantity >= 1
+    
+    Vậy khi tạo BOARD chú ý xác định LOCATION && POINT mà board đó thuộc về.
+
+*/
+
+// Define temp functions
+async function getRandomFormatId() {
+  const list = ["Cổ động chính trị", "Quảng cáo thương mại", "Xã hội hoá"];
+
+  let res = await AdvertisingFormat.findOne({
+    name: list[Math.floor(Math.random() * list.length)],
+  }).exec();
+  console.log("id:", res._id);
+  return res._id;
+}
+
+// Loop to create points
+let N = 50;
+let fullData = fs.readFileSync("./full.json", { encoding: "utf8" });
+fullData = JSON.parse(fullData);
+
+connectDB(process.env.MONGO_URI).then(() => {
+  fullData.forEach(async (point) => {
+    // Skip if ward or district is empty
+    if (point.ward == "" || point.district == "") return;
+
+    // Skip if district is not 1 or 3
+    const allowDistricts = ["1", "3", "10"];
+    if (!allowDistricts.includes(point.district)) return;
+
+    // Number of boards each point has (1-4)
+    let n_boards = Math.floor(Math.random() * 4) + 1;
+
+    // Create boards
+    let boards = [];
+    let pointObjectId = new mongoose.Types.ObjectId();
+    for (let i = 0; i < n_boards; i++) {
+      let newBoard = new AdvertisingBoard({
+        advertisingPoint: pointObjectId,
+        advertisingBoardType: AdvertisingBoard.schema.path(
+          "advertisingBoardType",
+        ).enumValues[Math.floor(Math.random() * 10)],
+        size: {
+          width: Math.floor(Math.random() * 10) + 1,
+          height: Math.floor(Math.random() * 10) + 1,
+        },
+        contractEndDate: new Date(
+          Date.now() + Math.floor(Math.random() * 1000000000),
+        ),
+      });
+      boards.push(newBoard._id);
+      newBoard.save();
+    }
+
+    // Create location
+    let newLocation = new LocationModel({
+      coords: {
+        lat: point.coords.lat,
+        lng: point.coords.lng,
+      },
+      locationName: point.title,
+      address: point.address,
+      ward: point.ward,
+      district: point.district,
+    });
+
+    let newPoint = new AdvertisingPoint({
+      _id: pointObjectId,
+      location: newLocation._id,
+      locationType:
+        AdvertisingPoint.schema.path("locationType").enumValues[
+          Math.floor(Math.random() * 6)
+        ],
+      advertisingFormat: await getRandomFormatId(),
+      planningStatus:
+        AdvertisingPoint.schema.path("planningStatus").enumValues[
+          Math.floor(Math.random() * 3)
+        ],
+      advertisingBoards: boards,
+    });
+
+    newLocation.save();
+    newPoint.save();
+  });
+});
