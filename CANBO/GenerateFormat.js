3ec7d163404113a6eb97e4be67d83177afcd4c57
Hai Nam <ddhnam21@clc.fitus.edu.vn>
2023-12-27 12:08:21 +0700

    add dev data generator files

--- CANBO/GenerateFormat.js
+++ CANBO/GenerateFormat.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const connectDB = require("./db/connect");
-const { AdvertisingFormat } = require("./models/Department/EnumList");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const connectDB = require('./db/connect');
+const { AdvertisingFormat } = require('./models/Department/EnumList');
+const mongoose = require('mongoose');
 
-const list = ["Cổ động chính trị", "Quảng cáo thương mại", "Xã hội hoá"];
+const list = ['Cổ động chính trị', 'Quảng cáo thương mại', 'Xã hội hoá'];
 
 connectDB(process.env.MONGO_URI).then(() => {
-  list.forEach((item) => {
-    AdvertisingFormat.create({ name: item });
-  });
+    list.forEach((item) => {
+        AdvertisingFormat.create({ name: item });
+    });
 });
