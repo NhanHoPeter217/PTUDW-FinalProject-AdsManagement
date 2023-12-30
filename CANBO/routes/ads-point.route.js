66120e86b37394febaaf3a67e0acdee9637ff447
Hồ Nhân <104899198+NhanHoPeter217@users.noreply.github.com>
2023-12-30 09:43:33 +0700

    view-engine-for-Ward: Done UI for manage AdsPoint

--- CANBO/routes/ads-point.route.js
+++ CANBO/routes/ads-point.route.js
@@ -6,4 +6,8 @@ router.route('/list').get(function (req, res) {
     res.render('vwAdsPoint/listAdsPoint');
 });
 
+router.route('/list/manage').get(function (req, res) {
+    res.render('vwAdsPoint/manageAdsPoint');
+});
+
 module.exports = router;
