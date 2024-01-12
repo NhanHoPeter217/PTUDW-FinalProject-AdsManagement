1a0bbd02d662949ad004a864a2ddcf2da9584fd0
Hai Nam <ddhnam21@clc.fitus.edu.vn>
2024-01-12 22:04:57 +0700

    AJAX Cho bản đồ người dân

--- NGUOIDAN/home.route.js
+++ NGUOIDAN/home.route.js
@@ -2,29 +2,29 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-async function getAllAdsPoints() {
-    try {
-        var result = await axios.get(`http://localhost:4000/adsPoint/allPoints/api/v1`);
-        let AdsPoints = result.data.adsPoints;
+// async function getAllAdsPoints() {
+//     try {
+//         var result = await axios.get(`http://localhost:4000/adsPoint/allPoints/api/v1`);
+//         let AdsPoints = result.data.adsPoints;
     
-        let AdsBoards = [];
+//         let AdsBoards = [];
 
-        AdsPoints.forEach((adsPoint) => {
-            AdsBoards.push(...adsPoint.adsBoard)
-        });
+//         AdsPoints.forEach((adsPoint) => {
+//             AdsBoards.push(...adsPoint.adsBoard)
+//         });
     
-        return { AdsPoints, AdsBoards };
-    }
-    catch (err) {
-        console.log(err);
-    }
+//         return { AdsPoints, AdsBoards };
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
     
-}
+// }
 
 router.get('/', async (req, res) => {
 
-    const { AdsPoints, AdsBoards } = await getAllAdsPoints();
+    // const { AdsPoints, AdsBoards } = await getAllAdsPoints();
 
-    res.render('home', { AdsPoints, AdsBoards });
+    res.render('home');
 });
 module.exports = router;
