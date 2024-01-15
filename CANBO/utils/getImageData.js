7668c4fdb763a4dc1ef7d525a2393494ca1d1a10
thang0306 <tdoannam980@gmail.com>
2024-01-15 16:48:38 +0700

    show Image updated

---
+++ CANBO/utils/getImageData.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+function getContentTypeFromExtension(extension) {
+    const contentTypeMap = {
+        'png': 'image/png',
+        'jpeg': 'image/jpeg',
+        'jpg': 'image/jpeg',
+        'pdf': 'application/pdf',
+    };
+
+    return contentTypeMap[extension.toLowerCase()] || 'application/octet-stream';
+}
+
+function getFileExtension(filePath) {
+    return path.extname(filePath).slice(1);
+}
+
+function getImageData(absolutePath, imagePath) {
+    const imageBuffer = fs.readFileSync(absolutePath);
+
+    return {
+        filename: path.basename(imagePath),
+        contentType: getContentTypeFromExtension(getFileExtension(imagePath)),
+        data: imageBuffer.toString('base64'),
+    };
+}
+
+module.exports = {
+    getImageData,
+};
