63dfde3440fd45a48950f4127eec4b99f0d4a692
thang0306 <tdoannam980@gmail.com>
2024-01-15 17:21:54 +0700

    update image of reports

--- CANBO/routes/WardAndDistrict/adsLicenseRequestRoutes.js
+++ CANBO/routes/WardAndDistrict/adsLicenseRequestRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const { authenticateUser, authorizePermissions } = require('../../middleware/authentication');
 
@@ -14,6 +15,11 @@ const {
 } = require('../../controllers/WardAndDistrict/adsLicenseRequestController');
 
 router.use(authenticateUser);
+
+const { configureUpload } = require('../../utils/handleFileUpload');
+const folderName = 'public/uploads/adsLicenseRequestImages'; 
+const maxImages = 5; 
+const upload = configureUpload(folderName, maxImages);
 
 router.route('/').get(authenticateUser, authorizePermissions('Sở VH-TT'), getAllAdsLicenseRequests);
 
@@ -35,7 +41,7 @@ router
 
 router.route('/assignedArea/:id').patch(updateAdsLicenseRequestByAssignedArea);
 router.route('/department/:id').patch(updateAdsLicenseRequestByDepartmentOfficier);
-router.route('/:id').post(authorizePermissions('Phường', 'Quận'), createAdsLicenseRequest);
+router.route('/:id').post(authorizePermissions('Phường', 'Quận'), upload, createAdsLicenseRequest);
 router.route('/:id').get(getSingleAdsLicenseRequest);
 
 module.exports = router;
